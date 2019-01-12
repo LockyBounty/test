31c0a066ba7eb3f021a09fa5884e671fe2bad317
locky <inox127@hotmail.com>
2019-01-12 03:44:42 +0100

    lol

--- JeuRoulette.py
+++ JeuRoulette.py
@@ -1,21 +1,22 @@
 import random    #importe le module random pour utiliser random
 import sys       #Pour utiliser module exit
+import math		
 
 
 
 print("\nJeu de la roulette")
 print("==================")
+print("Vous avez 100 euros en solde de jeu")
 print("Chiffre exact = gain x 3")
 print("Couleur exact = gain + 50%")
 print("--Chiffre pair= noir\n--Chiffre impair = rouge\n ")
 chiffre = input("Choisissez un chiffre entre 0 et 49 pour commencer : ")
 chiffre = int(chiffre)
+solde = 100  #solde fourni de base au jeu
 mise = input("Entrez votre mise : ")
 mise = int(mise)
-solde = 0
 gainNumero = 3
-gainCouleur = 0.5
-
+gainCouleur = 0.5 #50% de gains
 
 randomization = 0
 
@@ -27,24 +28,22 @@ def couleurPair(nb):
 
 if chiffre > 49 or chiffre < 0 :
     print("Numéro incorrect, apprends à lire !")
-    sys.exit()
+    sys.exit() #Si erreur, sortie du code
    
-    
-
-
 randomization = random.randrange(49)#retourne une valeur entre 0-49 inclus
 print("\nLa roulette s'est arrêtée à :", randomization, couleurPair(randomization))
-
 
 if chiffre == randomization :
     solde = mise * gainNumero
     print("Chiffre exact, solde actuel :", solde)
 elif couleurPair(chiffre) == couleurPair(randomization):
-    solde = mise + (mise*gainCouleur) 
-    print("Vous gagnez à la couleur, solde actuel :", solde)
+    solde = solde + mise*gainCouleur
+    print("Vous gagnez à la couleur, solde actuel :", round(solde))
 else : 
     solde = solde-mise
     print("Vous avez perdu votre mise, solde :", solde)
+	
+input("\nAppuyez sur Enter pour continuer...")
 
 
 
--- test.py
+++ test.py
@@ -1,4 +1,16 @@
-maListe = [1,2,3,4]
 
-for i, elt in enumerate(maListe):
-    print(elt)
+
+
+
+maListe = ["a", "b", "c", "d"]
+maListe.insert(2,'x')
+maListe2 = [1,2]
+
+maListe.extend(maListe2)
+
+
+print(maListe)
+
+input("bla bla...")
+
+
