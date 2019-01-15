5b9ed17a40f3be606b5fd6169bbe5c5f84896576
locky <bounthienglocky@gmail.com>
2019-01-15 12:22:39 +0100

    added lines

--- test.py
+++ test.py
@@ -1,6 +1,20 @@
-list1 = [1,2,3]
-list2 = list(list1)
-list2.append(5)
-print(list1, list2)
+class Personne:
+    def __init__(self, nom, prenom):
+        self.nom = nom
+        self.prenom = prenom
+        self.age = 33
+        self._lieu = "Bruxelles"
+    def _get_lieu(self):
+        return self._lieu
+    def _set_lieu(self, new_lieu):
+        print("Nouvelle résidence :", new_lieu)
+        self._lieu = new_lieu
+    lieu = property(_get_lieu, _set_lieu)
 
+
+locky = Personne("bounty", "locky")
+
+locky.lieu = "Amsterdam"
+
+#locky.lieu
 
